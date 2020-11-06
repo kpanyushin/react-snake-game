65dc7e30fa443c30b67e36fcfd9f3434fb2954f4
Kirill Panyushin <kapanyu1@mts.ru>
2020-11-06 14:21:19 +0300

    Inital setup

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import s from './App.module.scss';
+
+function App() {
+  return (
+    <div className={s.root}>
+      Snake Game
+    </div>
+  );
+}
+
+export default App;
